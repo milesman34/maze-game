ec9446a0b2aafb40813092bff6243fc4b49d410e
milesman34 <milesmanmusic@yahoo.com>
2023-05-04 16:16:04 -0400

    displays score

--- src/Game.js
+++ src/Game.js
@@ -2,74 +2,81 @@ import Player from "./Player";
 import { GameState } from "./enums";
 
 // This object handles the game state management
-const Game = state => ({
-    // Current game state
-    state,
+const Game = state => {
+    let object = {
+        // Current game state
+        state,
 
-    // Current level
-    level: null,
+        // Current level
+        level: null,
 
-    // Player object
-    player: null,
+        // Player object
+        player: null,
 
-    // Current score
-    score: 0,
+        // Current score
+        score: 0,
 
-    // Gets the current game state
-    getState() {
-        return this.state;
-    },
+        // Gets the current game state
+        getState() {
+            return this.state;
+        },
 
-    // Sets the new game state
-    setState(state) {
-        this.state = state;
-    },
+        // Sets the new game state
+        setState(state) {
+            this.state = state;
+        },
 
-    // Gets the current level
-    getLevel() {
-        return this.level;
-    },
+        // Gets the current level
+        getLevel() {
+            return this.level;
+        },
 
-    // Sets the current level
-    setLevel(level) {
-        this.level = level;
+        // Sets the current level
+        setLevel(level) {
+            this.level = level;
 
-        this.player = Player({
-            game: this,
-            position: this.level.getStartPos(),
-            level: this.level
-        });
+            this.player = Player({
+                game: this,
+                position: this.level.getStartPos(),
+                level: this.level
+            });
 
-        this.player.draw();
-    },
+            this.player.draw();
+        },
 
-    // Gets the current score
-    getScore() {
-        return this.score;
-    },
+        // Gets the current score
+        getScore() {
+            return this.score;
+        },
 
-    // Sets the current score
-    setScore(score) {
-        this.score = score;
-    },
+        // Sets the current score
+        setScore(score) {
+            this.score = score;
+            $("#score-container").text(`Score: ${score}`);
+        },
 
-    // Adds to the current score
-    addScore(amount) {
-        this.setScore(this.score + amount);
-        console.log(this.score);
-    },
+        // Adds to the current score
+        addScore(amount) {
+            this.setScore(this.score + amount);
+            console.log(this.score);
+        },
 
-    // Gets the current center offset for the level
-    getCenterOffset() {
-        return this.level.getCenterOffset();
-    },
+        // Gets the current center offset for the level
+        getCenterOffset() {
+            return this.level.getCenterOffset();
+        },
 
-    // Handles a keypress (pass event.key)
-    handleKeypress(key) {
-        if (this.state === GameState.Game) {
-            this.player.handleKeypress(key);
+        // Handles a keypress (pass event.key)
+        handleKeypress(key) {
+            if (this.state === GameState.Game) {
+                this.player.handleKeypress(key);
+            }
         }
     }
-});
+
+    object.setScore(0);
+
+    return object;
+};
 
 export default Game
