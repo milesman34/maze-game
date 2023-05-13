f92f86c48cf955c8f7c9c8900a18e21eef9cc895
milesman34 <milesmanmusic@yahoo.com>
2023-05-13 17:57:38 -0400

    work on mini level

--- src/game/levels/levels.ts
+++ src/game/levels/levels.ts
@@ -79,6 +79,46 @@ const levels = LevelTemplate.makeTemplates([
                 }
             }
         ])
+    },
+
+    {
+        name: "mini",
+        startingRoom: "start",
+        rooms: RoomTemplate.makeTemplates([
+            {
+                name: "start",
+                stringArray: [
+                    "AAAAAAAAAAAAAAA",
+                    "A  CCA BA   AMA",
+                    "AA AAA AA ACACA",
+                    "A    A CC ACCCA",
+                    "A AA AAAA AAAAA",
+                    "A BA L       CA",
+                    "AAAA AA AAA AAA",
+                    "A    AA CCA DEA",
+                    "A AACAA AAA AAA",
+                    "A KABAC      CA",
+                    "AAAAAAAAAAANAAA",
+                    "ACACAACA      A",
+                    "AMA    ACAAAA A",
+                    "A   AA CC ABC A",
+                    "AAAAAAAAAAAAAAA"
+                ],
+                charMap: {
+                    ...defCharMap,
+                    "E": LevelEnd(),
+                    "L": Lock({ color: colors.blue }),
+                    "K": Key({ color: colors.blue }),
+                    "D": Lock({ color: colors.red, amount: 2 }),
+                    "M": Key({ color: colors.red }),
+                    "N": Lock({ color: colors.red })
+                },
+                params: {
+                    startPos: Point(1, 1),
+                    scale: 1
+                }
+            }
+        ])
     }
     // {
     //     name: "locked",
